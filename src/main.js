14d2f3da17ecc6495f7163a9fa4277fa098f1b84
unknown <Romangergisevich@gmail.com>
2024-04-27 00:46:53 +0300

    form submit

--- src/main.js
+++ src/main.js
@@ -11,3 +11,51 @@ window.addEventListener("scroll", () => {
 upScrollBtn.addEventListener("click", () => {
   window.scrollTo(0, 0);
 });
+
+const form = document.querySelector("form");
+const userName = document.getElementById("userName");
+const site = document.getElementById("itemAdres");
+const consultation = document.getElementById("consultation");
+const rate = document.getElementById("rate");
+const access = document.getElementById("access");
+
+let consultationVal = false;
+let rateVal = false;
+let accessVal = false;
+
+consultation.addEventListener("change", (e) => {
+  if (e.target.checked) {
+    consultationVal = true;
+  } else {
+    consultationVal = false;
+  }
+});
+
+rate.addEventListener("change", (e) => {
+  if (e.target.checked) {
+    rateVal = true;
+  } else {
+    rateVal = false;
+  }
+});
+
+access.addEventListener("change", (e) => {
+  if (e.target.checked) {
+    accessVal = true;
+  } else {
+    accessVal = false;
+  }
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const result = {};
+  result.name = userName.value;
+  result.site = site.value;
+  result.description = taskDescription.value;
+  result.consultation = consultationVal;
+  result.rate = rateVal;
+  result.access = accessVal;
+
+  console.log(result);
+});
